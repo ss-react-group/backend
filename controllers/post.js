8102f5d6b33af5397e2af19e1e66f378e9d73388
franek <franek.stodulski@gmail.com>
2018-08-27 12:53:26 +0200

    added cors()

--- controllers/post.js
+++ controllers/post.js
@@ -38,8 +38,9 @@ function getAllPosts(req, res) {
     .then((allPosts) => {
       if (allPosts.length > 0) {
         res.status(200).send(allPosts);
+      } else {
+        res.status(404).send([]);
       }
-      res.status(404).send([]);
     })
     .catch(err => res.status(500).send(err));
 }
